fecb0b89e2508d496483b5d444da04890f79f52f
Richard Vernon Dyson <dyson77z@gmail.com>
2020-01-28 23:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ function nestedTarget() {
 }
 
 
-function increaseRankBy(n){
-  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
-
-  for (let i = 0; i < upRank.length; i++){
-    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
-    }
-
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
 }
-
 
 function deepestChild() {
   let gNode = document.querySelector('#grand-node')
